0a418d3a91214a67cb584b3b8f537563c2da2c12
juanmlinor <jumalire55@outlook.com>
2023-02-15 11:26:35 -0600

    Primer Commit

---
+++ Sales2023/Sales.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Sales.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
